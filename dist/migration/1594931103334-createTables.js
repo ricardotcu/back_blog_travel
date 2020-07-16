8bfd8e05a98ecd0e4b2f2258a46901a11c574035
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-16 17:41:54 -0300

    ajuste2

--- dist/migration/1594931103334-createTables.js
+++ dist/migration/1594931103334-createTables.js
@@ -18,16 +18,16 @@ class createTables1594931103334 {
         "titulo" character varying(100) NOT NULL, 
         "descricao" character varying(100) NOT NULL,  
         "caminho" character varying(100) NOT NULL,
-        CONSTRAINT "UQ_ba7f210baab523048c0386c3463" UNIQUE ("titulo"), 
-        CONSTRAINT "PK_139512aefbb11a5b2fa92696828" PRIMARY KEY ("id"))`);
+        CONSTRAINT "UQ_ca7f210baab523048c0386c3463" UNIQUE ("titulo"), 
+        CONSTRAINT "PK_239512aefbb11a5b2fa92696828" PRIMARY KEY ("id"))`);
     await queryRunner.query(`CREATE TABLE "comentario" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), 
         "descricao" character varying(100) NOT NULL,
-        CONSTRAINT "UQ_ba7f210baab523048c0386c3463" UNIQUE ("email"), 
-        CONSTRAINT "PK_139512aefbb11a5b2fa92696828" PRIMARY KEY ("id"))`);
+        CONSTRAINT "UQ_da7f210baab523048c0386c3463" UNIQUE ("email"), 
+        CONSTRAINT "PK_339512aefbb11a5b2fa92696828" PRIMARY KEY ("id"))`);
     await queryRunner.query(`CREATE TABLE "favorito" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), 
         "id_user" uuid NOT NULL,
-        CONSTRAINT "UQ_ba7f210baab523048c0386c3463" UNIQUE ("id_user"), 
-        CONSTRAINT "PK_139512aefbb11a5b2fa92696828" PRIMARY KEY ("id"))`);
+        CONSTRAINT "UQ_ea7f210baab523048c0386c3463" UNIQUE ("id_user"), 
+        CONSTRAINT "PK_439512aefbb11a5b2fa92696828" PRIMARY KEY ("id"))`);
   }
 
   async down(queryRunner) {}
