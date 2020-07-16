fd61dfbf1a3ab25da21280b81395b667729eaee9
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-16 20:06:02 -0300

    ajuste20

--- src/migration/1594937569546-addusers.ts
+++ src/migration/1594937569546-addusers.ts
@@ -4,7 +4,7 @@ export class addusers1594937569546 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-        `INSERT INTO "user" ("nome", "email", "senha") VALUES ('r', 'r@r.com', fd0eecba-2d33-4e94-a20f-bdb356e4d50d)`
+        `INSERT INTO "user" ("nome", "email", "senha") VALUES ("r", "r@r.com", 'fd0eecba-2d33-4e94-a20f-bdb356e4d50d')`
     );
     await queryRunner.query(
         `INSERT INTO user (nome, email, senha) VALUES ('ricardo', 'ricardo@ricardo.com', 0710da4b-f5a5-456e-9172-c12dcfa8455b)`
