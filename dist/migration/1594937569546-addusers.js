1fae704de10340378d5f240e754834c160c9dc77
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-16 19:36:04 -0300

    ajuste10

--- dist/migration/1594937569546-addusers.js
+++ dist/migration/1594937569546-addusers.js
@@ -7,9 +7,9 @@ exports.addusers1594937569546 = void 0;
 
 class addusers1594937569546 {
   async up(queryRunner) {
-    await queryRunner.query("INSERT INTO user (nome, email, senha) VALUES ('ricardo', 'ricardo@ricardo.com', 'ricardo')");
-    await queryRunner.query("INSERT INTO user (nome, email, senha) VALUES ('abc', 'abc@abc.com', 'abc')");
-    await queryRunner.query("INSERT INTO user (nome, email, senha) VALUES ('r', 'r@r.com', 'r')");
+    await queryRunner.query(`INSERT INTO user (nome, email, senha) VALUES ('r', 'r@r.com', 123456)`);
+    await queryRunner.query("INSERT INTO user (nome, email, senha) VALUES ('ricardo', 'ricardo@ricardo.com', 123456)");
+    await queryRunner.query("INSERT INTO user (nome, email, senha) VALUES ('abc', 'abc@abc.com', 123456)");
   }
 
   async down(queryRunner) {}
--- src/migration/1594937569546-addusers.ts
+++ src/migration/1594937569546-addusers.ts
@@ -2,17 +2,17 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class addusers1594937569546 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(
-            "INSERT INTO user (nome, email, senha) VALUES ('ricardo', 'ricardo@ricardo.com', 'ricardo')",
-        );
-        await queryRunner.query(
-            "INSERT INTO user (nome, email, senha) VALUES ('abc', 'abc@abc.com', 'abc')",
-        );
-        await queryRunner.query(
-            "INSERT INTO user (nome, email, senha) VALUES ('r', 'r@r.com', 'r')",
-        );
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+        `INSERT INTO user (nome, email, senha) VALUES ('r', 'r@r.com', 123456)`,
+    );
+    await queryRunner.query(
+        "INSERT INTO user (nome, email, senha) VALUES ('ricardo', 'ricardo@ricardo.com', 123456)",
+    );
+    await queryRunner.query(
+        "INSERT INTO user (nome, email, senha) VALUES ('abc', 'abc@abc.com', 123456)",
+    );
+  }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
     }
