1c81ace9d19e26603e11634b36f154e0a235f51f
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-16 18:56:14 -0300

    ajuste5

--- dist/migration/1594932401330-createralations.js
+++ dist/migration/1594932401330-createralations.js
@@ -7,10 +7,14 @@ exports.createralations1594932401330 = void 0;
 
 class createralations1594932401330 {
   async up(queryRunner) {
+    await queryRunner.query(`ALTER TABLE post ADD userId uuid NOT NULL;`);
+    await queryRunner.query(`ALTER TABLE favorito ADD userId uuid NOT NULL;`);
+    await queryRunner.query(`ALTER TABLE comentario ADD postId uuid NOT NULL;`);
+    await queryRunner.query(`ALTER TABLE comentario ADD userId uuid NOT NULL;`);
     await queryRunner.query(`ALTER TABLE "post" ADD CONSTRAINT "FK_2652456e912c983cde73d3281db" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     await queryRunner.query(`ALTER TABLE "favorito" ADD CONSTRAINT "FK_3652456e912c983cde73d3281db" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     await queryRunner.query(`ALTER TABLE "comentario" ADD CONSTRAINT "FK_4652456e912c983cde73d3281db" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    await queryRunner.query(`ALTER TABLE "comentario" ADD CONSTRAINT "FK_5652456e912c983cde73d3281db" FOREIGN KEY ("comentarioId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    await queryRunner.query(`ALTER TABLE "comentario" ADD CONSTRAINT "FK_5652456e912c983cde73d3281db" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
   }
 
   async down(queryRunner) {}
--- src/migration/1594932401330-createralations.ts
+++ src/migration/1594932401330-createralations.ts
@@ -4,6 +4,20 @@ export class createralations1594932401330 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
       await queryRunner.query(
+        `ALTER TABLE post ADD userId uuid NOT NULL;`
+      );
+      await queryRunner.query(
+        `ALTER TABLE favorito ADD userId uuid NOT NULL;`
+      );
+      await queryRunner.query(
+        `ALTER TABLE comentario ADD postId uuid NOT NULL;`
+      );
+      await queryRunner.query(
+        `ALTER TABLE comentario ADD userId uuid NOT NULL;`
+      );
+
+
+      await queryRunner.query(
         `ALTER TABLE "post" ADD CONSTRAINT "FK_2652456e912c983cde73d3281db" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
       );
       await queryRunner.query(
@@ -13,7 +27,7 @@ export class createralations1594932401330 implements MigrationInterface {
         `ALTER TABLE "comentario" ADD CONSTRAINT "FK_4652456e912c983cde73d3281db" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
       );
       await queryRunner.query(
-        `ALTER TABLE "comentario" ADD CONSTRAINT "FK_5652456e912c983cde73d3281db" FOREIGN KEY ("comentarioId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+        `ALTER TABLE "comentario" ADD CONSTRAINT "FK_5652456e912c983cde73d3281db" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
       );
     }
 
