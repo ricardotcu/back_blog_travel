3d6ae3e7248d90ff86d441b262a3dbc452d48a62
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-16 20:29:04 -0300

    ajuste24

---
+++ dist/migration/1594941567955-addposts.js
@@ -0,0 +1,185 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.addposts1594941567955 = void 0;
+
+class addposts1594941567955 {
+  async up(queryRunner) {
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        'fd0eecba-2d33-4e94-a20f-bdb356e4d50d')`);
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        'fd0eecba-2d33-4e94-a20f-bdb356e4d50d')`);
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        'fd0eecba-2d33-4e94-a20f-bdb356e4d50d')`);
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        'fd0eecba-2d33-4e94-a20f-bdb356e4d50d')`);
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        'fd0eecba-2d33-4e94-a20f-bdb356e4d50d')`);
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        'fd0eecba-2d33-4e94-a20f-bdb356e4d50d')`);
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        'fd0eecba-2d33-4e94-a20f-bdb356e4d50d')`);
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        '0710da4b-f5a5-456e-9172-c12dcfa8455b')`);
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        '4fc4b920-7350-4beb-9290-2647e527768e')`);
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        '4fc4b920-7350-4beb-9290-2647e527768e')`);
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        '4fc4b920-7350-4beb-9290-2647e527768e')`);
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        '4fc4b920-7350-4beb-9290-2647e527768e')`);
+    await queryRunner.query(`INSERT INTO "post" ("titulo", "descricao", "caminho", "userid") VALUES ('A Loving Heart is the Truest Wisdom', 
+        'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+        Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa 
+        voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos 
+        quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, 
+        quibusdam iste enim veniam, modi rem maiores.
+        Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor 
+        distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci 
+        modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur 
+        et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae 
+        numquam nam ab voluptatibus culpa, tenetur recusandae!', 
+        '../../assets/images/image_1.jpg', 
+        '4fc4b920-7350-4beb-9290-2647e527768e')`);
+  }
+
+  async down(queryRunner) {}
+
+}
+
+exports.addposts1594941567955 = addposts1594941567955;
