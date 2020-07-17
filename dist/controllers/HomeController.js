e1748408fde505125a7dd9b40d12463cedee3bf7
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-17 00:07:43 -0300

    ajuste16

--- dist/controllers/HomeController.js
+++ dist/controllers/HomeController.js
@@ -13,7 +13,9 @@ var _User = require("../entity/User");
 
 //retorna os produtos da home page, produtos marcados como mais vendidos
 const getHome = async (req, res) => {
-  const posts = await (0, _typeorm.getRepository)(_Post.Post).find();
+  const posts = await (0, _typeorm.getRepository)(_Post.Post).find({
+    relations: ["user"]
+  });
   return res.json(posts);
 }; //retorna os produtos da home page, produtos marcados como mais vendidos
 
--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -6,7 +6,7 @@ import { User } from '../entity/User';
 //retorna os produtos da home page, produtos marcados como mais vendidos
 export const getHome = async (req: Request, res: Response) => {
 
-    const posts = await getRepository(Post).find();
+    const posts = await getRepository(Post).find({ relations: ["user"] });
 
     return res.json(posts);
 }
