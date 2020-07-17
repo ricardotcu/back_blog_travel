05fb3c013bb9ff0e2c8074ff3633320b9ba3b892
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-16 23:01:50 -0300

    ajuste43

---
+++ src/migration/1594950910387-finalpopularposts.ts
@@ -0,0 +1,134 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class finalpopularposts1594950910387 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('A Loving xHeart is the Truest Wisdom', 
+            'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+            Nihilquod.', 
+            '../../assets/images/image_1.jpg', 
+            '982bb0eb-4749-4cab-9479-aa34d22d62ed')`
+        );
+        await queryRunner.query(
+          `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('A aLoving Heart is the Truest Wisdom', 
+          'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+          Nihilquod.', 
+          '../../assets/images/image_1.jpg', 
+          '982bb0eb-4749-4cab-9479-aa34d22d62ed')`
+        );
+        await queryRunner.query(
+          `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('A Loving Heafrt is the Truest Wisdom', 
+          'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+          Nihilquod.', 
+          '../../assets/images/image_1.jpg', 
+          '982bb0eb-4749-4cab-9479-aa34d22d62ed')`
+        );
+        await queryRunner.query(
+          `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('A Lovfing Heart is the Truest Wisdom', 
+          'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+          Nihilquod.', 
+          '../../assets/images/image_1.jpg', 
+          '982bb0eb-4749-4cab-9479-aa34d22d62ed')`
+        );
+        await queryRunner.query(
+          `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('A Loving Haeart is the Truest Wisdom', 
+          'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+          Nihilquod.', 
+          '../../assets/images/image_1.jpg', 
+          '982bb0eb-4749-4cab-9479-aa34d22d62ed')`
+        );
+        await queryRunner.query(
+          `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('A Loving Hearht is the Truest Wisdom', 
+          'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+          Nihilquod.', 
+          '../../assets/images/image_1.jpg', 
+          '7e8d05a7-0a19-4bfc-b9ad-fd1458c6ede2')`
+        );
+        await queryRunner.query(
+          `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('A Lovling Heart is the Truest Wisdom', 
+          'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+          Nihilquod.', 
+          '../../assets/images/image_1.jpg', 
+          '7e8d05a7-0a19-4bfc-b9ad-fd1458c6ede2')`
+        );
+        await queryRunner.query(
+          `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('A Lovinvg Heart is the Truest Wisdom', 
+          'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+          Nihilquod.', 
+          '../../assets/images/image_1.jpg', 
+          '7e8d05a7-0a19-4bfc-b9ad-fd1458c6ede2')`
+        );
+        await queryRunner.query(
+          `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('A Lovinfg Heart is the Truest Wisdom', 
+          'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+          Nihilquod.', 
+          '../../assets/images/image_1.jpg', 
+          '42d680d8-5895-43da-bafa-abfd1c113827')`
+        );
+        await queryRunner.query(
+          `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('A Loving Heart is the Tdruest Wisdom', 
+          'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+          Nihilquod.', 
+          '../../assets/images/image_1.jpg', 
+          '42d680d8-5895-43da-bafa-abfd1c113827')`
+        );
+        await queryRunner.query(
+          `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('A Lovinbg Heart is the Truest Wisdom', 
+          'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+          Nihilquod.', 
+          '../../assets/images/image_1.jpg', 
+          '42d680d8-5895-43da-bafa-abfd1c113827')`
+        );
+        await queryRunner.query(
+          `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('A Lobving Heart is the Truest Wisdom', 
+          'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+          Nihilquod.', 
+          '../../assets/images/image_1.jpg', 
+          '42d680d8-5895-43da-bafa-abfd1c113827')`
+        );
+        await queryRunner.query(
+          `INSERT INTO "post" ("titulo", "descricao", "caminho", "userId") VALUES ('Ab Loving Heart is the Truest Wisdom', 
+          'Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. 
+          Nihilquod.', 
+          '../../assets/images/image_1.jpg', 
+          '7e8d05a7-0a19-4bfc-b9ad-fd1458c6ede2')`
+        );
+
+
+        await queryRunner.query(
+            `INSERT INTO "favorito" ("userId") VALUES ('9594cd3d-7943-42db-8bc3-aaadc5a47c87')`
+          );
+          await queryRunner.query(
+            `INSERT INTO "favorito" ("userId") VALUES ('ed44f862-0663-4586-b0c8-3527be16736c')`
+          );
+          await queryRunner.query(
+            `INSERT INTO "favorito" ("userId") VALUES ('ed44f862-0663-4586-b0c8-3527be16736c')`
+          );
+          await queryRunner.query(
+            `INSERT INTO "favorito" ("userId") VALUES ('ed44f862-0663-4586-b0c8-3527be16736c')`
+          );
+          await queryRunner.query(
+            `INSERT INTO "favorito" ("userId") VALUES ('9594cd3d-7943-42db-8bc3-aaadc5a47c87')`
+          );
+          await queryRunner.query(
+            `INSERT INTO "favorito" ("userId") VALUES ('ed44f862-0663-4586-b0c8-3527be16736c')`
+          );
+          await queryRunner.query(
+            `INSERT INTO "favorito" ("userId") VALUES ('ed44f862-0663-4586-b0c8-3527be16736c')`
+          );
+          await queryRunner.query(
+            `INSERT INTO "favorito" ("userId") VALUES ('8f9f24b1-be1b-4639-b976-65b5d72d10c9')`
+          );
+          await queryRunner.query(
+            `INSERT INTO "favorito" ("userId") VALUES ('ed44f862-0663-4586-b0c8-3527be16736c')`
+          );
+          await queryRunner.query(
+            `INSERT INTO "favorito" ("userId") VALUES ('ed44f862-0663-4586-b0c8-3527be16736c')`
+          );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
