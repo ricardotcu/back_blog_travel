78ef5889eb15830a3df47abfa590c0a3ae4c9c2e
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-16 21:13:09 -0300

    ajuste31

--- src/migration/1594943748916-addfavoritos.ts
+++ src/migration/1594943748916-addfavoritos.ts
@@ -28,7 +28,7 @@ export class addfavoritos1594943748916 implements MigrationInterface {
           `INSERT INTO "favorito" ("id_user", "userid") VALUES ('9b687d81-f0a5-4acd-aa79-3a17630d3c99', '8f9f24b1-be1b-4639-b976-65b5d72d10c9')`
         );
         await queryRunner.query(
-          `INSERT INTO "favorito" ("id_user", "userid") VALUES ('9b687d81-f0b5-4acd-aa79-3a17630d8c99', 'ed44f862-0663-4586-b0c8-3527be16736c')`
+          `INSERT INTO "favorito" ("id_user", "userid") VALUES ('9b687d81-f0b5-4acd-aa79-8a17630d8c99', 'ed44f862-0663-4586-b0c8-3527be16736c')`
         );
         await queryRunner.query(
           `INSERT INTO "favorito" ("id_user", "userid") VALUES ('9b687d81-f0a5-4acd-aa79-3a17r30d8c99', 'ed44f862-0663-4586-b0c8-3527be16736c')`
