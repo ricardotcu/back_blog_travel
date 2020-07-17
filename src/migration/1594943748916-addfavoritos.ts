e06277ec5c05956e9b490e7855c8e36354d0b85e
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-16 21:05:23 -0300

    ajuste29

--- src/migration/1594943748916-addfavoritos.ts
+++ src/migration/1594943748916-addfavoritos.ts
@@ -4,34 +4,34 @@ export class addfavoritos1594943748916 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-          `INSERT INTO "favorito" ("id_user") VALUES ('8f9f24b1-be1b-4639-b976-65b5d72d10c9')`
+          `INSERT INTO "favorito" ("id_user", "userid") VALUES ('8f9df24b1-be1b-4639-b976-65b5d72d10c9', '9594cd3d-7943-42db-8bc3-aaadc5a47c87')`
         );
         await queryRunner.query(
-          `INSERT INTO "favorito" ("id_user") VALUES ('8f9f24b1-be1b-4639-b976-65b5d72d10c9')`
+          `INSERT INTO "favorito" ("id_user", "userid") VALUES ('8f9f24b1-bea1b-4639-b976-65b5d72d10c9', 'ed44f862-0663-4586-b0c8-3527be16736c')`
         );
         await queryRunner.query(
-          `INSERT INTO "favorito" ("id_user") VALUES ('8f9f24b1-be1b-4639-b976-65b5d72d10c9')`
+          `INSERT INTO "favorito" ("id_user", "userid") VALUES ('8f9f24b1-be1gb-4639-b976-65ab5d72d10c9', 'ed44f862-0663-4586-b0c8-3527be16736c')`
         );
         await queryRunner.query(
-          `INSERT INTO "favorito" ("id_user") VALUES ('9594cd3d-7943-42db-8bc3-aaadc5a47c87')`
+          `INSERT INTO "favorito" ("id_user", "userid") VALUES ('9594cd3d-7943-42dgb-8bc3-aaadc5a47c87', 'ed44f862-0663-4586-b0c8-3527be16736c')`
         );
         await queryRunner.query(
-          `INSERT INTO "favorito" ("id_user") VALUES ('9594cd3d-7943-42db-8bc3-aaadc5a47c87')`
+          `INSERT INTO "favorito" ("id_user", "userid") VALUES ('9594cd3d-7943-42db-8bc3-aaadcn5a47c87', '9594cd3d-7943-42db-8bc3-aaadc5a47c87')`
         );
         await queryRunner.query(
-          `INSERT INTO "favorito" ("id_user") VALUES ('8f9f24b1-be1b-4639-b976-65b5d72d10c9')`
+          `INSERT INTO "favorito" ("id_user", "userid") VALUES ('8f9f24b1-be1b-463r9-b976-65b5d72d10c9', 'ed44f862-0663-4586-b0c8-3527be16736c')`
         );
         await queryRunner.query(
-          `INSERT INTO "favorito" ("id_user") VALUES ('ed44f862-0663-4586-b0c8-3527be16736c')`
+          `INSERT INTO "favorito" ("id_user", "userid") VALUES ('ed44f862-0663-458h6-b0c8-3527be16736c', 'ed44f862-0663-4586-b0c8-3527be16736c')`
         );
         await queryRunner.query(
-          `INSERT INTO "favorito" ("id_user") VALUES ('9594cd3d-7943-42db-8bc3-aaadc5a47c87')`
+          `INSERT INTO "favorito" ("id_user", "userid") VALUES ('9594cd3d-7943-42db-8bc3-aaadc5a47c87', '8f9f24b1-be1b-4639-b976-65b5d72d10c9')`
         );
         await queryRunner.query(
-          `INSERT INTO "favorito" ("id_user") VALUES ('8f9f24b1-be1b-4639-b976-65b5d72d10c9')`
+          `INSERT INTO "favorito" ("id_user", "userid") VALUES ('8f9f24b1-beae1b-4639-b976-65b5d72d10c9', 'ed44f862-0663-4586-b0c8-3527be16736c')`
         );
         await queryRunner.query(
-          `INSERT INTO "favorito" ("id_user") VALUES ('ed44f862-0663-4586-b0c8-3527be16736c')`
+          `INSERT INTO "favorito" ("id_user", "userid") VALUES ('ed44f862-0663-4586-b0c8-3527hebe16736c', 'ed44f862-0663-4586-b0c8-3527be16736c')`
         );
     }
 
