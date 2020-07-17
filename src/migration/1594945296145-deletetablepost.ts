c8adc753f62e2443a4a5ee2c0d7f124e86d991a9
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-16 21:22:31 -0300

    ajuste33

---
+++ src/migration/1594945296145-deletetablepost.ts
@@ -0,0 +1,12 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class deletetablepost1594945296145 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        `DROP TABLE post`
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
