0ebae199d4cdb96836679d234d6e449ab29d265b
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-18 05:16:35 -0300

    ajuste37

--- dist/migration/1595056156369-addcomentarios.js
+++ dist/migration/1595056156369-addcomentarios.js
@@ -14,27 +14,27 @@ class addcomentarios1595056156369 {
     await queryRunner.query(`CREATE INDEX "IDX_ccb4ae4609bfbf4d022560a3f8" ON "favpost" ("userId") `);
     await queryRunner.query(`CREATE INDEX "IDX_9a616bdfff1c46bddeb8ad78db" ON "favpost" ("postId") `);
     await queryRunner.query(`INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentario comentario 
-      comentario comentario comentario comentario comentario comentario comentario ', 
+      comentario comentario comentario comentariocomentariocomentariocontario', 
       '9a8a0c88-475f-485c-b944-abb11bf19a10',
       '666c0df3-4af7-48f7-ab3e-54a703fee1a6')`);
     await queryRunner.query(`INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentario comentario 
-      comentario comentario comentario comentario comentario comentario comentario ', 
+      comentario comentario comentario comentario comentario comeiocomentario', 
       '7e8d05a7-0a19-4bfc-b9ad-fd1458c6ede2',
       '666c0df3-4af7-48f7-ab3e-54a703fee1a6')`);
     await queryRunner.query(`INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentario comentario 
-      comentario comentario comentario comentario comentario comentario comentario ', 
+      comentario comentario comentario comentario comentario comentario comen', 
       '7e8d05a7-0a19-4bfc-b9ad-fd1458c6ede2',
       '42d680d8-5895-43da-bafa-abfd1c113827')`);
-    await queryRunner.query(`INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentario comentario 
-      comentario comentario comentario comentario comentario comentario comentario ', 
+    await queryRunner.query(`INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentariocomenrio 
+      comentario comentario comentario comentario comentario comentario comtario', 
       '7e8d05a7-0a19-4bfc-b9ad-fd1458c6ede2',
       '666c0df3-4af7-48f7-ab3e-54a703fee1a6')`);
     await queryRunner.query(`INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentario comentario 
-      comentario comentario comentario comentario comentario comentario comentario ', 
+      comentario comentario comentario comentario comentario comentarientario', 
       'daea73a1-4aea-4f93-a567-b793a733e3e0',
       '666c0df3-4af7-48f7-ab3e-54a703fee1a6')`);
     await queryRunner.query(`INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentario comentario 
-      comentario comentario comentario comentario comentario comentario comentario ', 
+      comentario comentario comentario comentario comentario comentao comeaio', 
       'daea73a1-4aea-4f93-a567-b793a733e3e0',
       '42d680d8-5895-43da-bafa-abfd1c113827')`);
     await queryRunner.query(`INSERT INTO "favpost" ("userId", "postId") VALUES ('666c0df3-4af7-48f7-ab3e-54a703fee1a6',
--- src/migration/1595056156369-addcomentarios.ts
+++ src/migration/1595056156369-addcomentarios.ts
@@ -28,37 +28,37 @@ export class addcomentarios1595056156369 implements MigrationInterface {
 
     await queryRunner.query(
       `INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentario comentario 
-      comentario comentario comentario comentario comentario comentario comentario ', 
+      comentario comentario comentario comentariocomentariocomentariocontario', 
       '9a8a0c88-475f-485c-b944-abb11bf19a10',
       '666c0df3-4af7-48f7-ab3e-54a703fee1a6')`
     );
     await queryRunner.query(
       `INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentario comentario 
-      comentario comentario comentario comentario comentario comentario comentario ', 
+      comentario comentario comentario comentario comentario comeiocomentario', 
       '7e8d05a7-0a19-4bfc-b9ad-fd1458c6ede2',
       '666c0df3-4af7-48f7-ab3e-54a703fee1a6')`
     );
     await queryRunner.query(
       `INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentario comentario 
-      comentario comentario comentario comentario comentario comentario comentario ', 
+      comentario comentario comentario comentario comentario comentario comen', 
       '7e8d05a7-0a19-4bfc-b9ad-fd1458c6ede2',
       '42d680d8-5895-43da-bafa-abfd1c113827')`
     );
     await queryRunner.query(
-      `INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentario comentario 
-      comentario comentario comentario comentario comentario comentario comentario ', 
+      `INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentariocomenrio 
+      comentario comentario comentario comentario comentario comentario comtario', 
       '7e8d05a7-0a19-4bfc-b9ad-fd1458c6ede2',
       '666c0df3-4af7-48f7-ab3e-54a703fee1a6')`
     );
     await queryRunner.query(
       `INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentario comentario 
-      comentario comentario comentario comentario comentario comentario comentario ', 
+      comentario comentario comentario comentario comentario comentarientario', 
       'daea73a1-4aea-4f93-a567-b793a733e3e0',
       '666c0df3-4af7-48f7-ab3e-54a703fee1a6')`
     );
     await queryRunner.query(
       `INSERT INTO "comentario" ("descricao", "postId", "userId") VALUES ('comentario comentario 
-      comentario comentario comentario comentario comentario comentario comentario ', 
+      comentario comentario comentario comentario comentario comentao comeaio', 
       'daea73a1-4aea-4f93-a567-b793a733e3e0',
       '42d680d8-5895-43da-bafa-abfd1c113827')`
     );
